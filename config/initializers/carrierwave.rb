182975f8d472a58d3c5e49ac25fa1026162e64dd
Kizzonia <chibuikefortune@hotmail.com>
2021-01-16 12:44:23 +0800

    push

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,12 +8,12 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                     # required
+    :aws_access_key_id      => 'AKIAJ5UFI6KSULXTSPGA',                        # required
+    :aws_secret_access_key  => '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',                     # required
     :endpoint => 'https://nyc3.digitaloceanspaces.com',
     :region => 'nyc3'
   }
-  config.fog_directory  = ENV['S3_BUCKET_NAME']                             # required
+  config.fog_directory  = jaiz                             # required
   config.fog_public     = true                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
